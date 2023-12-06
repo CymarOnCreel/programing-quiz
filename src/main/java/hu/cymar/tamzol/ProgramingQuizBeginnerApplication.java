f7773bd57a3c644a6f4654d485d7fb0951d65395
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-12-06 16:31:50 +0100

    Project created using SpringBoot Tool Suite4

---
+++ src/main/java/hu/cymar/tamzol/ProgramingQuizBeginnerApplication.java
@@ -0,0 +1,13 @@
+package hu.cymar.tamzol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramingQuizBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramingQuizBeginnerApplication.class, args);
+	}
+
+}
