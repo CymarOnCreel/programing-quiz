6e67d5e84fe9a4682986eecf7a46987b39858bed
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-12-06 16:58:34 +0100

    Upload database sql-s
    creating databases sql
    added a dummy data createor sql for development testing

---
+++ src/main/resources/static/database/dummy_data.sql
@@ -0,0 +1,53 @@
+INSERT INTO question_categories (category_name) VALUES
+('Java'), ('Python');
+
+INSERT INTO question_subcategories (subcategory_name, category_id) VALUES
+('Basic Syntax', 1),
+('Variables', 1),
+('Loops', 1),
+('Object-Oriented Programming', 1),
+('Testing', 1);
+INSERT INTO questions (question_text, category_id, subcategory_id) VALUES
+ ('What is Java?', 1, 1),
+('How to declare a variable in Java?', 1, 2),
+('What is a for loop?', 1, 3),
+('Explain the concept of inheritance.', 1, 4),
+('What is JUnit?', 1, 5);
+INSERT INTO answers (answer_text,question_id, is_correct) VALUES
+-- Question 1
+('A programming language', 1, true),
+('A data structure', 1, false),
+('A framework for machine learning', 1, false),
+('An operating system', 1, false),
+
+-- Question 2
+('int x = 10;', 2, true),
+('int x == 10;', 2, false),
+('int x := 10;', 2, false),
+('Declare x = 10;', 2, false),
+
+-- Question 3
+('A loop that iterates a specific number of times.', 3, true),
+('A way to achieve code reusability through class relationships.', 3, false),
+('A design pattern in object-oriented programming.', 3, false),
+('A type of data structure.', 3, false),
+
+-- Question 4
+('A Java testing framework.', 4, true),
+('A JavaScript library for building user interfaces.', 4, false),
+('A Python web framework.', 4, false),
+('A database management system.', 4, false),
+
+-- Question 5
+('A unit testing framework for Java.', 5, true),
+('A version control system for Java projects.', 5, false),
+('A Java framework for building web applications.', 5, false),
+('A programming language for mobile app development.', 5, false);
+
+
+INSERT INTO users (firstname, lastname, username, password, email, user_category) VALUES
+('Teszt', 'Elek', 'teszt_elek', 'password123', 'teszt.elek@quiz.com','USER'),
+('Nap', 'Pali', 'nap_pali', '123password', 'nap.pali@quiz.com','ADMIN');
+INSERT INTO answer_scores (user_id, category_id, subcategory_id, quest_count, right_answer_count) VALUES
+(1, 1, 1, 5, 3),
+(2, 1, 2, 5, 4);
---
+++ src/main/resources/static/database/quiz_app_schema.sql
@@ -0,0 +1,60 @@
+CREATE SCHEMA `quiz` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci ;
+
+Use quiz;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(80) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    user_category ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER'
+);
+
+-- Question Category Table
+CREATE TABLE question_categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(70) NOT NULL
+);
+
+-- Question Subcategory Table
+CREATE TABLE question_subcategories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    subcategory_name VARCHAR(70) NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES question_categories(id)
+);
+
+-- Questions Table
+CREATE TABLE questions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    question_text TEXT NOT NULL,
+    category_id INT,
+    subcategory_id INT,
+    FOREIGN KEY (category_id) REFERENCES question_categories(id),
+    FOREIGN KEY (subcategory_id) REFERENCES question_subcategories(id)
+);
+
+-- Answers Table
+CREATE TABLE answers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    answer_text TEXT NOT NULL,
+    question_id INT,
+    is_correct BOOLEAN NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
+-- Answer Score Table
+CREATE TABLE answer_scores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    category_id INT,
+    subcategory_id INT,
+    quest_count INT,
+    right_answer_count INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES question_categories(id),
+    FOREIGN KEY (subcategory_id) REFERENCES question_subcategories(id)
+);
