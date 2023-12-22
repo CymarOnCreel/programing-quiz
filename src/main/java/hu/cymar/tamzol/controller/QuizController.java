36f1c52990b5f9bd5bf17f2a10a90bdf1a1a5a89
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-12-22 17:12:04 +0100

    added gettallquestions and get questions based on number inputet by user to quiz generation

--- src/main/java/hu/cymar/tamzol/controller/QuizController.java
+++ src/main/java/hu/cymar/tamzol/controller/QuizController.java
@@ -1,6 +1,8 @@
 package hu.cymar.tamzol.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpSession;
 
@@ -14,33 +16,42 @@ import hu.cymar.tamzol.model.Answer;
 import hu.cymar.tamzol.model.Question;
 import hu.cymar.tamzol.model.QuestionCategory;
 import hu.cymar.tamzol.model.QuestionSubcategory;
+import hu.cymar.tamzol.repository.QuestionRepository;
 import hu.cymar.tamzol.service.QuizService;
 
 @Controller
 public class QuizController {
-	   private final QuizService quizService;
+	private final QuizService quizService;
 
-	    @Autowired
-	    public QuizController(QuizService quizService) {
-	        this.quizService = quizService;
-	    }
+	@Autowired
+	public QuizController(QuizService quizService) {
+		this.quizService = quizService;
+	}
 
 	@PostMapping("/quiz")
 	public String getFilteredQuestions(Model model, HttpSession session,
-			@RequestParam(name = "selectedSubcategories", required = false) List<QuestionSubcategory> subcategories) {
+			@RequestParam(name = "selectedSubcategories", required = false) List<QuestionSubcategory> subcategories,
+			@RequestParam(name = "numQuestions", defaultValue = "1") int numberOfQuestionsToGenerate,
+			@RequestParam(name = "action", defaultValue = "") String action) {
 		if (subcategories == null || subcategories.isEmpty()) {
 			return "redirect:/selectSubcategories";
 		}
 		QuestionCategory selectedCategory = (QuestionCategory) session.getAttribute("selectedCategory");
 		session.setAttribute("subcategories", subcategories);
 		model.addAttribute("selectedSubcategories", subcategories);
-		List<Question>filteredQuestions=quizService.getFilteredQuestions(selectedCategory, subcategories, model);
-		List<List<Answer>>answers=quizService.getAnswersList(model, filteredQuestions);
+		System.out.println(numberOfQuestionsToGenerate + "mindenelott");
+
+		if ("getAllQuestions".equals(action)) {
+			List<Question> allQuestions = quizService.getAllQuestions();
+			numberOfQuestionsToGenerate = allQuestions.size();
+		}
+		List<Question> filteredQuestions = quizService.getFilteredQuestions(selectedCategory, subcategories, model,
+				numberOfQuestionsToGenerate);
+		List<List<Answer>> answers = quizService.getAnswersList(model, filteredQuestions);
 		session.setAttribute("filteredQuestions", filteredQuestions);
-	    session.setAttribute("answers", answers);
+		session.setAttribute("answers", answers);
+
 		return "quiz";
 	}
-
-
 
 }
